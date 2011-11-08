abaa5369cbd5496d04c6a58fa67676304066583b
Nick Markwell <nick@duckinator.net>
2011-11-07 20:32:04 -0500

    Cleaned up main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,3 @@
-function update_github() {
-	$('#github').text('');
-	github('duckinator', '#github');
-}
-
 function update_twitter() {
 	$('#twitter').text('');
 	twitter('duckinator', '#twitter');
@@ -13,23 +8,11 @@ $(document).ready(function(){
 	rot_all();
 	mailfix();
 
-	if ($('#twitter')) {
+	/*if ($('#twitter')) {
 		// This is so people w/out JS don't see a random "Twitter" on the right
 		$('#twitter_header').text('Twitter');
 		update_twitter(4); // Initial update
 		setInterval(update_twitter, 120000);  // Every 2  minutes
-	}
-
-	if ($('#github')) {
-		$('#github_message').text("My 10 most recently active personal projects:");
-		update_github();  // Initial update
-		setInterval(update_github,  1200000); // Every 20 minutes
-	}
+	}*/
 });
 
-Konami.init(function(){
-    if ($('body')[0].getAttribute('class') == 'konami')
-        $('body')[0].setAttribute('class','');
-    else
-        $('body')[0].setAttribute('class','konami');
-});
